cff35057ea6838d72c432a8f4bf9f61def222b50
Srikanth Srinivas <Srikanth@Srikanths-iMac-2.local>
2017-12-29 20:58:27 -0500

    first commit

---
+++ PayNPark/Model/ParkingArea.swift
@@ -0,0 +1,9 @@
+//
+//  ParkingArea.swift
+//  PayNPark
+//
+//  Created by Srikanth Srinivas on 12/29/17.
+//  Copyright © 2017 Srikanth Srinivas. All rights reserved.
+//
+
+import Foundation
---
+++ PayNPark/Model/ParkingSpot.swift
@@ -0,0 +1,9 @@
+//
+//  ParkingSpot.swift
+//  PayNPark
+//
+//  Created by Srikanth Srinivas on 12/29/17.
+//  Copyright © 2017 Srikanth Srinivas. All rights reserved.
+//
+
+import Foundation
