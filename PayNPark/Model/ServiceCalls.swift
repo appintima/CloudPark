3e0deaade8244733c2cd7755c30294aba670a5c0
Me <Srikanth@srikanths-imac-2.local>
2017-12-29 21:05:31 -0500

    commit

--- PayNPark/Model/ServiceCalls.swift
+++ PayNPark/Model/ServiceCalls.swift
@@ -11,75 +11,88 @@ import Foundation
 import UIKit
 import CoreLocation
 import Firebase
-import Mapbox
 
 //Singleton
 class ServiceCalls{
+    
 
     private var fireBaseRef: DatabaseReference!
-    private let jobsRef: DatabaseReference!
-    private let needConfirmRef: DatabaseReference!
+    private let parkingAreaRef: DatabaseReference!
+    private let parkingSpotRef: DatabaseReference!
     private let userRef: DatabaseReference!
-    var availableJobs: [Job] = []
+    var availableSpots: [ParkingSpot] = []
+    var parkingAreaCode: String?
     
     init() {
+        
         fireBaseRef = Database.database().reference()
-        jobsRef = fireBaseRef.child("AllJobs")
+        parkingAreaRef = fireBaseRef.child("ParkingArea")
         userRef = fireBaseRef.child("Users")
-        needConfirmRef = fireBaseRef.child("NeedConfirmationJobs")
+        parkingSpotRef = parkingAreaRef.child("ParkingSpot")
         
         
+    }
+    
+    func getParkingAreaCode(completion: @escaping (String)->()){
+        
+        Auth.auth().signInAnonymously(completion: nil)
+        parkingAreaRef.observe(.value) { (snapshot) in
+            
+            let code = snapshot.value
+            self.parkingAreaCode = code as? String
+            completion (self.parkingAreaCode!)
+
+        }
     }
     
     
     func addJobToFirebase(jobTitle: String, jobDetails: String, pricePerHour: String, numberOfHours: String, locationCoord: CLLocationCoordinate2D){
         
-        let user = Auth.auth().currentUser
-        let newJobID = self.jobsRef.childByAutoId().key
-        let jobOwnerEmailHash = self.MD5(string: (Auth.auth().currentUser?.email)!)
-        let latitude = locationCoord.latitude
-        let longitude = locationCoord.longitude
-        
-        let jobDict: [String:Any] = ["latitude":latitude, "longitude":longitude, "JobOwner":jobOwnerEmailHash, "JobTitle":jobTitle, "JobDescription":jobDetails, "Price":pricePerHour, "Time":numberOfHours, "isOccupied":false, "isCompleted":false, "Full Name":(user?.displayName)!]
-        self.jobsRef.child(newJobID).updateChildValues(jobDict)
-        
-        
-        
-        // adding job to the user who posted list of posted jobs
-        let userPostedRef = self.userRef.child(self.MD5(string: (user?.email)!)).child("PostedJobs")
-        userPostedRef.child(newJobID).updateChildValues(jobDict)
-        
+//        let user = Auth.auth().currentUser
+//        let newJobID = self.jobsRef.childByAutoId().key
+//        let jobOwnerEmailHash = self.MD5(string: (Auth.auth().currentUser?.email)!)
+//        let latitude = locationCoord.latitude
+//        let longitude = locationCoord.longitude
+//
+//        let jobDict: [String:Any] = ["latitude":latitude, "longitude":longitude, "JobOwner":jobOwnerEmailHash, "JobTitle":jobTitle, "JobDescription":jobDetails, "Price":pricePerHour, "Time":numberOfHours, "isOccupied":false, "isCompleted":false, "Full Name":(user?.displayName)!]
+//        self.jobsRef.child(newJobID).updateChildValues(jobDict)
+//
+//
+//
+//        // adding job to the user who posted list of posted jobs
+//        let userPostedRef = self.userRef.child(self.MD5(string: (user?.email)!)).child("PostedJobs")
+//        userPostedRef.child(newJobID).updateChildValues(jobDict)
+//
     }
     
     
-    
-    func getJobFromFirebase(completion: @escaping ([Job],[MGLPointAnnotation])->()){
-
-        var newJobs : [Job] = []
-        var annotations = [MGLPointAnnotation]()
-        jobsRef.observe(.childAdded, with: { (snapshot) in
-            
-            let job = Job(snapshot: snapshot as! DataSnapshot)
-            self.userRef.observe(.value, with: { (snapshot2) in
-                let userIDs = snapshot2.value as! [String : AnyObject]
-                job.jobOwnerRating = userIDs[job.jobOwnerEmailHash]!["Rating"] as! Float
-
-                if job.jobOwnerEmailHash != self.MD5(string: (Auth.auth().currentUser?.email)!){
-                    newJobs.append(job)
-                    
-                    let point = MGLPointAnnotation()
-                    point.coordinate = job.location.coordinate
-                    point.title = job.title
-                    point.subtitle = ("$"+"\(job.wage_per_hour)"+"/Hour")
-                    annotations.append(point)
-                }
-                
-                
-                print(newJobs, job.title)
-                completion(newJobs, annotations)
-            })
-            
-        })
+//
+//    func getJobFromFirebase(completion: @escaping ([Job],[MGLPointAnnotation])->()){
+//
+////        var spots : [ParkingSpot] = []
+////        jobsRef.observe(.childAdded, with: { (snapshot) in
+////
+////            let job = Job(snapshot: snapshot as! DataSnapshot)
+////            self.userRef.observe(.value, with: { (snapshot2) in
+////                let userIDs = snapshot2.value as! [String : AnyObject]
+////                job.jobOwnerRating = userIDs[job.jobOwnerEmailHash]!["Rating"] as! Float
+////
+////                if job.jobOwnerEmailHash != self.MD5(string: (Auth.auth().currentUser?.email)!){
+////                    newJobs.append(job)
+////
+////                    let point = MGLPointAnnotation()
+////                    point.coordinate = job.location.coordinate
+////                    point.title = job.title
+////                    point.subtitle = ("$"+"\(job.wage_per_hour)"+"/Hour")
+////                    annotations.append(point)
+////                }
+////
+////
+////                print(newJobs, job.title)
+////                completion(newJobs, annotations)
+////            })
+//
+//        })
 //
 //        jobsRef.observe(.childAdded, with: { (snapshot) in
 //
@@ -108,79 +121,79 @@ class ServiceCalls{
 //        })
     }
     
-    
-    func acceptPressed(job: Job, user: User, completion: @escaping (String)->()){
-        let applicantRef = self.needConfirmRef.child(job.jobID!).child("Applicants")
-        applicantRef.child(self.MD5(string: user.email!)).setValue(user.displayName)
-
-        self.needConfirmRef.child(job.jobID).child("JobTitle").setValue(job.title)
-        self.needConfirmRef.child(job.jobID).child("JobDescription").setValue(job.description)
-        self.needConfirmRef.child(job.jobID).child("JobOwner").setValue(job.jobOwnerEmailHash)
-        self.needConfirmRef.child(job.jobID).child("Full Name").setValue(job.jobOwnerFullName)
-        self.needConfirmRef.child(job.jobID).child("Price").setValue("\(job.wage_per_hour)")
-        self.needConfirmRef.child(job.jobID).child("Time").setValue("\(job.maxTime)")
-        self.needConfirmRef.child(job.jobID).child("latitude").setValue(job.latitude)
-        self.needConfirmRef.child(job.jobID).child("longitude").setValue(job.longitude)
-        self.needConfirmRef.child(job.jobID).child("isCompleted").setValue(false)
-        self.needConfirmRef.child(job.jobID).child("isOccupied").setValue(false)
-        applicantRef.child(self.MD5(string: user.email!)).setValue(user.displayName)
-        
-        
-        
-        let userAcceptedRef = self.userRef.child(self.MD5(string: user.email!)).child("AcceptedJobs")
-        userAcceptedRef.child(job.jobID).child("JobTitle").setValue(job.title)
-        userAcceptedRef.child(job.jobID).child("JobDescription").setValue(job.description)
-        userAcceptedRef.child(job.jobID).child("JobOwner").setValue(job.jobOwnerEmailHash)
-        userAcceptedRef.child(job.jobID).child("Full Name").setValue(job.jobOwnerFullName)
-        userAcceptedRef.child(job.jobID).child("Price").setValue("\(job.wage_per_hour)")
-        userAcceptedRef.child(job.jobID).child("Time").setValue("\(job.maxTime)")
-        userAcceptedRef.child(job.jobID).child("latitude").setValue(job.latitude)
-        userAcceptedRef.child(job.jobID).child("longitude").setValue(job.longitude)
-        userAcceptedRef.child(job.jobID).child("isCompleted").setValue(false)
-        userAcceptedRef.child(job.jobID).child("isOccupied").setValue(false)
-        
-        let jobOwnerEmailHash = job.jobOwnerEmailHash!
-        userRef.observe(.value, with: { (snapshot) in
-            let userValues = snapshot.value as! [String : AnyObject]
-            guard let deviceToken = userValues[jobOwnerEmailHash]!["currentDevice"]! as? String else{return}
-            completion(deviceToken)
-            self.userRef.removeAllObservers()
-        })
-        
-    }
-    
-    
-    func loadUncomfirmedJobs(completion: @escaping ([Job])->()) {
-        needConfirmRef.observe(.value, with: { (snapshot) in
-            var uncomfirmedJobs: [Job] = []
-            for item in snapshot.children{
-                let job = Job(snapshot: item as! DataSnapshot)
-                if job.jobOwnerEmailHash == self.MD5(string: (Auth.auth().currentUser?.email)!){
-                    uncomfirmedJobs.append(job)
-                }
-                
-            }
-            
-            completion(uncomfirmedJobs)
-            self.needConfirmRef.removeAllObservers()
-        })
-    }
-    
-    
-    func MD5(string: String) -> String {
-        let messageData = string.data(using:.utf8)!
-        var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
-        
-        _ = digestData.withUnsafeMutableBytes {digestBytes in
-            messageData.withUnsafeBytes {messageBytes in
-                CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
-            }
-        }
-        
-        return digestData.map { String(format: "%02hhx", $0) }.joined()
-    }
-    
-}
+//
+//    func acceptPressed(job: Job, user: User, completion: @escaping (String)->()){
+//        let applicantRef = self.needConfirmRef.child(job.jobID!).child("Applicants")
+//        applicantRef.child(self.MD5(string: user.email!)).setValue(user.displayName)
+//
+//        self.needConfirmRef.child(job.jobID).child("JobTitle").setValue(job.title)
+//        self.needConfirmRef.child(job.jobID).child("JobDescription").setValue(job.description)
+//        self.needConfirmRef.child(job.jobID).child("JobOwner").setValue(job.jobOwnerEmailHash)
+//        self.needConfirmRef.child(job.jobID).child("Full Name").setValue(job.jobOwnerFullName)
+//        self.needConfirmRef.child(job.jobID).child("Price").setValue("\(job.wage_per_hour)")
+//        self.needConfirmRef.child(job.jobID).child("Time").setValue("\(job.maxTime)")
+//        self.needConfirmRef.child(job.jobID).child("latitude").setValue(job.latitude)
+//        self.needConfirmRef.child(job.jobID).child("longitude").setValue(job.longitude)
+//        self.needConfirmRef.child(job.jobID).child("isCompleted").setValue(false)
+//        self.needConfirmRef.child(job.jobID).child("isOccupied").setValue(false)
+//        applicantRef.child(self.MD5(string: user.email!)).setValue(user.displayName)
+//
+//
+//
+//        let userAcceptedRef = self.userRef.child(self.MD5(string: user.email!)).child("AcceptedJobs")
+//        userAcceptedRef.child(job.jobID).child("JobTitle").setValue(job.title)
+//        userAcceptedRef.child(job.jobID).child("JobDescription").setValue(job.description)
+//        userAcceptedRef.child(job.jobID).child("JobOwner").setValue(job.jobOwnerEmailHash)
+//        userAcceptedRef.child(job.jobID).child("Full Name").setValue(job.jobOwnerFullName)
+//        userAcceptedRef.child(job.jobID).child("Price").setValue("\(job.wage_per_hour)")
+//        userAcceptedRef.child(job.jobID).child("Time").setValue("\(job.maxTime)")
+//        userAcceptedRef.child(job.jobID).child("latitude").setValue(job.latitude)
+//        userAcceptedRef.child(job.jobID).child("longitude").setValue(job.longitude)
+//        userAcceptedRef.child(job.jobID).child("isCompleted").setValue(false)
+//        userAcceptedRef.child(job.jobID).child("isOccupied").setValue(false)
+//
+//        let jobOwnerEmailHash = job.jobOwnerEmailHash!
+//        userRef.observe(.value, with: { (snapshot) in
+//            let userValues = snapshot.value as! [String : AnyObject]
+//            guard let deviceToken = userValues[jobOwnerEmailHash]!["currentDevice"]! as? String else{return}
+//            completion(deviceToken)
+//            self.userRef.removeAllObservers()
+//        })
+//
+//    }
+//
+//
+//    func loadUncomfirmedJobs(completion: @escaping ([Job])->()) {
+//        needConfirmRef.observe(.value, with: { (snapshot) in
+//            var uncomfirmedJobs: [Job] = []
+//            for item in snapshot.children{
+//                let job = Job(snapshot: item as! DataSnapshot)
+//                if job.jobOwnerEmailHash == self.MD5(string: (Auth.auth().currentUser?.email)!){
+//                    uncomfirmedJobs.append(job)
+//                }
+//
+//            }
+//
+//            completion(uncomfirmedJobs)
+//            self.needConfirmRef.removeAllObservers()
+//        })
+//    }
+//
+//
+//    func MD5(string: String) -> String {
+//        let messageData = string.data(using:.utf8)!
+//        var digestData = Data(count: Int(CC_MD5_DIGEST_LENGTH))
+//
+//        _ = digestData.withUnsafeMutableBytes {digestBytes in
+//            messageData.withUnsafeBytes {messageBytes in
+//                CC_MD5(messageBytes, CC_LONG(messageData.count), digestBytes)
+//            }
+//        }
+//
+//        return digestData.map { String(format: "%02hhx", $0) }.joined()
+//    }
+//
+//}
 
 
 
--- PayNPark/ViewController/PayNPark.swift
+++ PayNPark/ViewController/PayNPark.swift
@@ -7,14 +7,108 @@
 //
 
 import UIKit
+import Pastel
+import Lottie
+import Material
+import PopupDialog
+import Firebase
 
-class ViewController: UIViewController {
+class PayNPark: UIViewController {
 
+    @IBOutlet weak var checkAnimation: UIView!
+    @IBOutlet weak var GoButton: RaisedButton!
+    @IBOutlet weak var ParkingCode: TextField!
+    @IBOutlet weak var LogoAnimation: UIView!
+    @IBOutlet weak var GradientView: PastelView!
+    let QRCodeLogo = LOTAnimationView(name: "cloud")
+    var parkingCodeFromFirebase: String!
+    let CheckLogo = LOTAnimationView(name: "simple_tick")
+    let serviceCalls = ServiceCalls()
+    
+    
     override func viewDidLoad() {
+        
+        prepareGoButton()
+        prepareTitleTextField()
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.navigationBar.isHidden = true
+        GradientView.setColors([#colorLiteral(red: 0.05296700448, green: 0.1554270983, blue: 0.3375893831, alpha: 1),#colorLiteral(red: 0.2513618469, green: 0.5344406962, blue: 1, alpha: 1),#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)])
+        GradientView.animationDuration = 3.0
+        LogoAnimation.handledAnimation(Animation: QRCodeLogo)
+        QRCodeLogo.play()
+        QRCodeLogo.loopAnimation = true
+        checkAnimation.handledAnimation(Animation: CheckLogo)
+        checkAnimation.isHidden = true
+        self.hideKeyboardWhenTappedAround()
+        serviceCalls.getParkingAreaCode { (code) in
+            self.parkingCodeFromFirebase = code
+            print(self.parkingCodeFromFirebase)
+        }
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        GradientView.startAnimation()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        GradientView.startAnimation()
+        
+    }
+    
+    @IBAction func goButtonPressed(_ sender: Any) {
+        if ParkingCode.isEmpty{
+            let codeErrorPopup = preparePopupForWrongCode()
+            self.present(codeErrorPopup, animated: true, completion: nil)
+            
+        }
+        
+        else if ParkingCode.text == parkingCodeFromFirebase{
+            checkAnimation.isHidden = false
+            GoButton.isHidden = true
+            CheckLogo.play()
+        }
+        
+        else{
+            let codeErrorPopup = preparePopupForWrongCode()
+            self.present(codeErrorPopup, animated: true, completion: nil)
+        }
+    }
+    
+    
+    func prepareGoButton(){
+        
+        self.GoButton.image = Icon.cm.check
+    }
+    
+    func preparePopupForWrongCode() -> PopupDialog{
+        
+        let popupTitle = "Error"
+        let popupMessage = "You have entered an incorrect parking code"
+        let wrongCodePopup = PopupDialog(title: popupTitle, message: popupMessage)
+        return wrongCodePopup
+    }
+    
+    func prepareTitleTextField(){
+        
+        self.ParkingCode.dividerNormalColor = Color.white
+        self.ParkingCode.dividerActiveColor = Color.white
+        self.ParkingCode.placeholderLabel.font = UIFont(name: "Century Gothic", size: 17)
+        self.ParkingCode.font = UIFont(name: "Century Gothic", size: 17)
+        self.ParkingCode.textColor = Color.white
+        self.ParkingCode.placeholder = "Parking Code"
+        self.ParkingCode.placeholderActiveColor = Color.white
+        self.ParkingCode.placeholderNormalColor = Color.white
+        self.ParkingCode.detail = "Enter your parking area code"
+        self.ParkingCode.detailColor = Color.white
+    }
+    
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
